eac9a6cf0b17b91db97cf8f0a61ef4b001b413be
Emmanuel Ashekun <ashekunemmanuel@gmail.com>
2023-11-02 19:10:27 +0000

    #13 migrate local state to terraform cloud using terraform block

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
+
+
 terraform {
+  cloud {
+    organization = "emmyash-demo1"
+
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
   required_providers {
     random = {
       source  = "hashicorp/random"

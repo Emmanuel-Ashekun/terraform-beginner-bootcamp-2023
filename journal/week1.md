87f51b562c2023ba63151ac6a267c72a280f75de
Emmanuel Ashekun <ashekunemmanuel@gmail.com>
2023-11-07 01:59:35 +0000

    #20 change structure of root module and add tagging

--- journal/week1.md
+++ journal/week1.md
@@ -17,3 +17,28 @@ Our root module structure is as follows:
 
 
 [Standard Module Structure](https://developer.hashicorp.com/terraform/language/modules/develop/structure)
+
+##
+
+### Terraform Cloud Variables
+
+In terraform we can set two kinds
+- Environment- bash terminal
+- Terraform - tfvars 
+
+### Loading Terraform Variables
+
+we can use the `-var` to set ann input variable or override a variable in the tfvar file
+
+### var-file flah
+- [TODO] : research this file
+
+### terraform.tfvars
+
+This is the default file to load in terraform variables in bulk
+
+### auto.tfvars
+- TODO: Document
+
+### order of terraform variables
+- TODO: Document which takes precedence
--- main.tf
+++ main.tf
@@ -1,35 +1,3 @@
-
-
-terraform {
-  cloud {
-    organization = "emmyash-demo1"
-
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.23.1"
-    }
-  }
-}
-
-
-provider "aws" {
-  # Configuration options
-}
-
-
-provider "random" {
-  # Configuration options
-}
-# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
   length  = 16
   special = false
@@ -40,12 +8,9 @@ resource "aws_s3_bucket" "example" {
   bucket = random_string.bucket_name.result
 
   tags = {
-    Name        = "My bucket"
+    UserUuid        = var.user_uuid
     Environment = "Dev"
   }
 }
 
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
-}
-
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
